07e50abced1f6d752c23a8325c7543dd3d8c3b9a
Scott Ellis <scott@jumpnowtek.com>
2022-04-17 04:46:28 -0400

    u-boot-scr: Mount rootfs rw

--- recipes-bsp/u-boot-scr/files/boot.cmd
+++ recipes-bsp/u-boot-scr/files/boot.cmd
@@ -1,6 +1,6 @@
 setenv bootpart 0:2
 setenv bootdir /boot
-setenv mmcroot /dev/mmcblk0p2 ro
+setenv mmcroot /dev/mmcblk0p2 rw
 setenv mmcrootfstype ext4 rootwait
 setenv bootargs console=${console} root=${mmcroot} rootfstype=${mmcrootfstype}
 setenv fdtfile omap3-overo-storm-tobi.dtb
